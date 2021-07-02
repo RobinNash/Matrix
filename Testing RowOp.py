29534b977e41a1f87b2f5c7d442cb49062966d94
Robin Nash <84207317+RobinNash@users.noreply.github.com>
2021-07-02 19:10:21 -0400

    Create Testing RowOp.py

---
+++ Testing RowOp.py
@@ -0,0 +1,27 @@
+# Testing RowOp object of matrix.py module #
+# and Matrix functions implementing RowOp  #
+# By Robin Nash
+from matrix import *
+
+A = Matrix('''1 0 1 1
+1 1 1 4
+2 1 1 2''')
+
+# init
+ops = [RowOp(0,1,2),RowOp(1,1,2),RowOp(2,1,2,3)]
+for op in ops:
+    print(op)
+print()
+
+# testing invert function of RowOp
+optests = ["R2-3/2R3","R2sR3","R2*-3/4"]
+for op in optests:
+    op = RowOp(op)
+    print(op,op.invert(),sep=', ')
+print()
+
+# RowOp on matrix
+optests = ["R2+R1","R1sR3","R3*-2"]
+for opst in optests:
+    A.row_op(opst).print()
+    print()
